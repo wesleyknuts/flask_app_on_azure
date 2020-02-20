c2999e710b475e738965336c04950a03c05d8fe5
wesleyknuts <46556463+wesleyknuts@users.noreply.github.com>
2020-02-20 21:42:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
-from json import dumps
-# from flask.ext.jsonpify import jsonify
-
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,10 +8,6 @@ class index(Resource):
     def get(self):
          return "Azure Python Demo"
         
-
 api.add_resource(index, '/index') # Route_1
 
 
-
-if __name__ == '__main__':
-     app.run(port='5002')
